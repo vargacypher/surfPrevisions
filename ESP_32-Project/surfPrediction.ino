af5163f99ec4ccd6c7b187d37bb964557b1a9d9f
Guilherme Cardoso de Vargas <77084039+vargacypher@users.noreply.github.com>
2024-02-04 23:02:07 -0300

    Update surfPrediction.ino

--- ESP_32-Project/surfPrediction.ino
+++ ESP_32-Project/surfPrediction.ino
@@ -9,10 +9,11 @@ TFT_eSPI lcd = TFT_eSPI();
 TFT_eSprite sprite = TFT_eSprite(&lcd);
 TFT_eSprite spr = TFT_eSprite(&lcd); //sprite for seconds
 TFT_eSprite spr2 = TFT_eSprite(&lcd); //sprite for calendar
+TFT_eSprite spr3 = TFT_eSprite(&lcd); //sprite for waves
 TFT_eSprite cal = TFT_eSprite(&lcd);
 
-const char* ssid     = "dasdasdasdasdasd-NW";
-const char* password = "dasdsadfdgf32432afhg";
+const char* ssid     = "Vargas-NW";
+const char* password = "vavg2011nw";
 
 const char* ntpServer = "pool.ntp.org";
 const long  gmtOffset_sec = -3 * 3600;             //time zone * 3600 , my time zone is  +1 GTM
@@ -26,53 +27,54 @@ String apiUrl = "https://raw.githubusercontent.com/vargacypher/surfPrevisions/ma
 #define purple 0x604D
 #define green 0x1AE9
 
-String IP;
 
 
-// int left=0;
-// int right=14;
+void setup(void){    
 
-void setup(void)
-{    
 
-    // pinMode(left,INPUT_PULLUP);
-    // pinMode(right,INPUT_PULLUP);
-    lcd.init();
-    lcd.setRotation(1);
-    sprite.createSprite(320, 170);
-    sprite.setSwapBytes(true);
-    sprite.setTextColor(TFT_WHITE,0xEAA9);
-    sprite.setTextDatum(4);
+  lcd.init();
+  lcd.setRotation(1);
+  sprite.createSprite(320, 170);
+  sprite.setSwapBytes(true);
+  sprite.setTextColor(TFT_WHITE,0xEAA9);
+  sprite.setTextDatum(4);
 
-    cal.createSprite(218,31);
-    cal.fillSprite(TFT_GREEN);
-    cal.setTextColor(TFT_WHITE,TFT_GREEN);
+  cal.createSprite(218,31);
+  cal.fillSprite(TFT_GREEN);
+  cal.setTextColor(TFT_WHITE,TFT_GREEN);
+  cal.setFreeFont(&Orbitron_Light_24);
 
-    spr.createSprite(80,64);
-    //spr.fillSprite(TFT_GREEN);
-    spr2.createSprite(100,100);
-    spr2.setSwapBytes(true);
-    spr2.fillSprite(TFT_GREEN);
-    spr2.setTextDatum(4);
-    spr.setTextColor(TFT_WHITE,TFT_GREEN);
-    spr2.setTextColor(TFT_WHITE,TFT_GREEN);
-    spr.setFreeFont(&Orbitron_Light_32);
-    spr2.setFreeFont(&Orbitron_Light_24);
-    
-   lcd.fillScreen(TFT_BLACK);
-   
-   Serial.begin(9600);
-   lcd.print("Connecting to ");
-   lcd.println(ssid);
-   WiFi.begin(ssid, password);
-   while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    lcd.print(".");
-    Serial.print(".");
-   }
-   Serial.println("");
-   Serial.println("WiFi connected.");
-   IP=WiFi.localIP().toString();
+  spr.createSprite(120,250);
+  spr.fillSprite(TFT_GREEN);
+  spr2.createSprite(100,100);
+  spr2.setSwapBytes(true);
+  spr2.fillSprite(TFT_GREEN);
+  spr2.setTextDatum(4);
+  spr.setTextColor(TFT_WHITE,TFT_GREEN);
+  spr2.setTextColor(TFT_WHITE,TFT_GREEN);
+  spr.setFreeFont(&Orbitron_Light_32);
+  spr2.setFreeFont(&Orbitron_Light_24);
+  
+
+  spr3.createSprite(120,250);
+  spr3.fillSprite(TFT_GREEN);
+  spr3.setTextColor(TFT_WHITE,TFT_GREEN);
+  spr3.setFreeFont(&Orbitron_Light_24);
+
+
+  lcd.fillScreen(TFT_BLACK);
+  
+  Serial.begin(9600);
+  lcd.print("Connecting to ");
+  lcd.println(ssid);
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+  delay(500);
+  lcd.print(".");
+  Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected.");
 
   configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
 
@@ -86,51 +88,58 @@ void setup(void)
 
 int temp=0;
 int x=0;
-long startF=0;
-long endF=0;
-double fps=0;
-
 long t=0;
 int xt=230;
 int yt=8;
 
 
-void loop()
-{
-  startF=millis();
-  
+void loop(){  
   spr.fillSprite(TFT_GREEN);
   spr2.fillSprite(TFT_GREEN);
+  spr3.fillSprite(TFT_GREEN);
   spr2.setFreeFont(&Orbitron_Light_24);
   sprite.pushImage(0,0,aniWidth,aniHeigth,logo2[x]);
   delay(80);
 
+  //clock - date
   sprite.setTextColor(purple,TFT_WHITE);
-
+  sprite.fillRoundRect(234,3,65,26,3,TFT_WHITE);
+  sprite.drawString(String(timeHour)+":"+String(timeMin),xt+36,yt+3,2);
+  sprite.drawString(String(day)+"/"+String(month),265,23,1);
   
-  sprite.fillRoundRect(234,3,65,28,3,TFT_WHITE);
-  sprite.drawString(String(timeHour)+":"+String(timeMin),xt+36,yt+5,2);
-  sprite.drawString(String(day)+"/"+String(month),265,25,1);
+  //Temp and Windy
+  sprite.setTextColor(TFT_BLACK);
+  sprite.drawString(String("25")+"C",40,150,2);
+  sprite.setTextColor(TFT_WHITE);
+  sprite.drawString("N",173,118,2);
+  sprite.drawString(String("25")+"km/h",170,165,1);
 
-  spr2.drawString("Pico de Mares",52,2,2);
+
+  // Tides
+  spr.drawString("Pico de Mares",8,5,2);
   // High tids
-  spr2.drawString(String("0.5")+"m",1,40,2);
+  spr.drawString(String("0.5")+"m/"+String("9")+"h",0,56,2);
   // low tids
-  spr2.drawString(String("0.2")+"m",9,40,2);
+  spr.drawString(String("0.2")+"m/"+String("15")+"h",59,56,2);
 
+  //Waves
+  spr3.drawString(String("Swell e Periodo"),12,35,2);
+  spr3.drawString(String("1.0m")+"/"+String("15")+"h",8,75,2);
+  spr3.drawString("-",73,75,2);
+  spr3.drawString(String("10")+"s",85,75,2);
 
-
+  //Main title
   cal.drawRoundRect(0,0,150,28,4,TFT_WHITE);
-  cal.drawString("Pvisao Ondas e Mares",7,4,2);
+  cal.drawString("Ondas e Mares",20,4,1);
  
 
-
   cal.pushToSprite(&sprite,xt-224,yt,TFT_GREEN);
-  //spr.pushToSprite(&sprite,xt,yt+70+16+6,TFT_GREEN);
+  spr.pushToSprite(&sprite,192,yt+16+6,TFT_GREEN);
   spr2.pushToSprite(&sprite,217,55,TFT_GREEN);
+  spr3.pushToSprite(&sprite,195,yt+60+6,TFT_GREEN);
   sprite.pushSprite(0,0);
 
-
+  //Update clock
   if(t+1000<millis()){
   printLocalTime();
   t=millis();
@@ -140,7 +149,5 @@ void loop()
   if(x==framesNumber)
   x=0;
   
-  endF=millis();
-  fps=1000/(endF-startF);
 }
 

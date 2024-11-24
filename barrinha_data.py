5902ff19292421ea8fdd48603da926b05ba4bad2
Guilherme Cardoso de Vargas <77084039+vargacypher@users.noreply.github.com>
2024-11-24 18:00:39 -0300

    fix spotid

--- barrinha_data.py
+++ barrinha_data.py
@@ -2,6 +2,6 @@ import json
 from api.fetch_data import generate_data
 
 if __name__ == "__main__":
-    surf_data = generate_data('5842041f4e65fad6a7708cfd')
+    surf_data = generate_data('640a5822b6d769477c573bf3')
     with open('barrinha.json', 'w') as f:
         json.dump(surf_data.__dict__, f)

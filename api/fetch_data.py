f286cdf0b056e8164000d51a4c9b55437c29fb3a
Guilherme Cardoso de Vargas <77084039+vargacypher@users.noreply.github.com>
2024-11-24 17:59:10 -0300

    fix metrics

--- api/fetch_data.py
+++ api/fetch_data.py
@@ -65,15 +65,20 @@ def generate_data(spotId:str):
 
     surf_data = DailySurf(
         dt_coleted = general_df[0]['timestamp_dt'],
-        surf_waves_min = round((general_df[0]['surf_min']*30.48)/100,2),
-        surf_waves_max =  '-'.join([str(round((general_df[0]['surf_min']*30.48)/100,2)),str(round((general_df[0]['surf_max']*30.48)/100,2))]), #Feet to cm
+        surf_waves_min = round(general_df[0]['surf_min'],2),
+        # surf_waves_min = round((general_df[0]['surf_min']*30.48)/100,2),
+        surf_waves_max =  '-'.join([str(round(general_df[0]['surf_min'],2)),str(round(general_df[0]['surf_max'],2))]), #Feet to cm
+        # surf_waves_max =  '-'.join([str(round((general_df[0]['surf_min']*30.48)/100,2)),str(round((general_df[0]['surf_max']*30.48)/100,2))]), #Feet to cm
         probability = general_df[0]['probability'],
         wind_direction = general_df[0]['direction'],
         wind_speed = str(general_df[0]['speed']),
-        general_temp = str(round((general_df[0]['temperature'] - 32) / 1.8, 2)), # Fahrenheit Celsius
-        max_tideshigh = str(round((tide_max[0]['height']*30.48)/100,2)), #Feet to cm
+        general_temp = str(round(general_df[0]['temperature'] , 2)), # Fahrenheit Celsius
+        # general_temp = str(round((general_df[0]['temperature'] - 32) / 1.8, 2)), # Fahrenheit Celsius
+        max_tideshigh = str(round(tide_max[0]['height'],2)), #Feet to cm
+        # max_tideshigh = str(round((tide_max[0]['height']*30.48)/100,2)), #Feet to cm
         timestamp_max_tideshigh = tide_max[0]['timestamp_dt'],
-        min_tideslow = str(round((tide_min[0]['height']*30.48)/100,2)), #Feet to cm
+        min_tideslow = str(round(tide_min[0]['height'],2)), #Feet to cm
+        # min_tideslow = str(round((tide_min[0]['height']*30.48)/100,2)), #Feet to cm
         timestamp_min_tideslow = tide_min[0]['timestamp_dt']
     )
 
